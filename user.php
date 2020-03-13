16b5488e19e7d4cc092a910db9c90bca5ad76d9b
joyamadea <amadeajoy@gmail.com>
2020-03-13 14:29:03 +0700

    edited some icons

--- user.php
+++ user.php
@@ -22,11 +22,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    
+    
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+    <!-- <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css"> -->
+    
 </head>
 <body>
     <div class="container">
@@ -64,19 +68,20 @@
         <div class="row">
             
             <?php 
-                $query2="SELECT * FROM post WHERE username='$username'";
+                $query2="SELECT * FROM post";
                 $result2=$db->query($query2) or die($db->error);
             
                 while($row1=$result2->fetch_assoc()){
                     $caption=$row1['teks'];
                     $picture=$row1['gambar'];
                     $post_id=$row1['post_id'];
+                    $uname=$row1['username'];
 
                     echo "<div class='card'>";
                         
                         echo "<div class='card-body'>";
                             echo "<div class='card-title'>";
-                                echo $username;
+                                echo $uname;
                                 echo "<button type='button' style='float: right' class='btn btn-danger' data-toggle='modal' data-target='#delete'>Delete</button>";
                                 echo "</div>";                                  
                             echo "<img src='".$picture."'/ width='500px;'>";
@@ -106,7 +111,8 @@
                 <div class="alert alert-danger">Are you sure you want delete this post?</div>
             </div>    
             <div class="modal-footer">
-                <button type="submit" name="delete" class="btn btn-danger">Yes</button>
+                
+                <button type="submit" name="delete" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</button>
                 <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
             </div>
             </form>
