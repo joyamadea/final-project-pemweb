3c1a10df58c9cc35cd2287fa371a06567baa830e
owenrch <62130920+owenrch@users.noreply.github.com>
2020-03-18 22:13:04 +0700

    Update login.php

--- login.php
+++ login.php
@@ -6,15 +6,15 @@ include 'connect.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$data = mysqli_query($koneksi,"select * from admin where username='$username' and password='$password'");
+$data = mysqli_query($connect,"select * from users where username='$username' and password='$password'");
 
 $check = mysqli_num_rows($data);
 
 if($check > 0){
 	$_SESSION['username'] = $username;
 	$_SESSION['status'] = "login";
-	header("location:admin/index.php");
+	header("location:index.php");
 }else{
-	header("location:index.php?pesan=gagal");
+	header("location:login.php?pesan=gagal");
 }
 ?>
