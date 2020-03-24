2f7df5fd2ce8c3f43a3371aeda1e77c6f5501a79
joyamadea <amadeajoy@gmail.com>
2020-03-25 01:54:31 +0700

    config setup

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php
-    $host = "localhost";
-	$dbuname = "root";
-	$dbname ="customer";
-	$password = "";
+	//uts ver
+    // $host = "localhost";
+	// $dbuname = "root";
+	// $dbname ="customer";
+	// $password = "";
+	// $db = mysqli_connect($host, $dbuname, $password, $dbname);
+
+	//deploy ver
+	$host = "us-cdbr-iron-east-01.cleardb.net";
+	$dbuname = "b56e908223081f";
+	$dbname ="heroku_455d3e6ecc52e25";
+	$password = "72a473f9";
 	$db = mysqli_connect($host, $dbuname, $password, $dbname);
-	
-	//this is a comment
 ?>
